f9aa4c301faa2b7160d9cb71766911da92d07ff3
Alexandr Roslyakov <99288384+KingDog25@users.noreply.github.com>
2022-02-09 02:24:10 +0300

    Add files via upload

---
+++ AccumulationOfSums.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace _23_variant_CSharp_1_3_2
+{
+    partial class AccumulationOfSums
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableName = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox1.ImageLocation = "C:\\Users\\Александр\\source\\repos\\23_variant_CSharp_1_3_2\\images\\Formula1.png";
+            this.pictureBox1.Location = new System.Drawing.Point(641, 498);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(220, 95);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.WaitOnLoad = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9.163636F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(349, 551);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 42);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Рассчитать значения";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // tableName
+            // 
+            this.tableName.AutoSize = true;
+            this.tableName.Font = new System.Drawing.Font("Bell MT", 15.70909F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tableName.Location = new System.Drawing.Point(309, 498);
+            this.tableName.Name = "tableName";
+            this.tableName.Size = new System.Drawing.Size(260, 27);
+            this.tableName.TabIndex = 5;
+            this.tableName.Text = "Таблица значений функции";
+            this.tableName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dataGridView1.RowHeadersWidth = 47;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(959, 492);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // AccumulationOfSums
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(959, 605);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tableName);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "AccumulationOfSums";
+            this.Text = "Накопление сумм и произведений";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label tableName;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ AccumulationOfSums.cs
@@ -0,0 +1,82 @@
+﻿/*Накопление сумм и произведений*/
+/*1.3.2  Составить циклический алгоритм и программу для вычисления результата по формуле.
+ * Для проверки программы задать X = 0,5 ; n = 20*/
+
+/*Accumulation of sums and products*/
+/*1.3.2 Write a cyclic algorithm and a program for calculating the result using a formula.
+  * To test the program set X = 0.5 ; n = 20*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _23_variant_CSharp_1_3_2
+{
+    public partial class AccumulationOfSums : Form
+    {
+        public AccumulationOfSums()
+        {
+            InitializeComponent();
+            
+        }
+
+
+        public static double Prelim()                                                 //функция для предварительного расчета
+        {
+            int n = 20;
+            double x = 0.5;
+            double prelim = Math.Sin(Math.PI * Convert.ToDouble(n) / (x + 3));
+            return prelim;
+        }
+
+
+
+        public static double functValTable(double x, double k)
+        {
+            double y;
+            y = Prelim() * Math.Sqrt(Math.Pow(Math.Pow(x, k-1)+Math.Pow(Math.Exp(k-3.0/2.0), 1.0 / k), 1.0 / k) / 1.0 + Math.Log(x));
+            return y;
+        }
+
+
+
+        private void tableName_Text(object sender, EventArgs e)
+        {
+            tableName.Text = "Таблица значений функции";
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int n = 20;
+            double x = 0.5;
+            double[,] tableXYk = new double[20, 3];
+            dataGridView1.RowCount = n;                                //кол-во строк
+            dataGridView1.ColumnCount = 3;                               //столбцов
+            dataGridView1.Columns[0].HeaderText = "Шаг (k)";
+            dataGridView1.Columns[1].HeaderText = "X";
+            dataGridView1.Columns[2].HeaderText = "Y";
+            string[] tableY = new string[20];
+
+            for (int count_row = 0; count_row < n; count_row++)
+            {
+                for (int count_column = 0; count_column < dataGridView1.ColumnCount; count_column++)                                         //заполняем значения X
+                {
+                    tableXYk[count_row, 0] = count_row+1;                                      //заполняем массив случайными значениями координат
+                    dataGridView1.Rows[count_row].Cells[0].Value = "k = " + tableXYk[count_row, 0];
+
+                    tableXYk[count_row, 1] = x;                                             //X
+                    dataGridView1.Rows[count_row].Cells[1].Value = tableXYk[count_row, 1];
+
+                    tableY[count_row] = functValTable(x, count_row+1).ToString("0.####");                                             //Y
+                    dataGridView1.Rows[count_row].Cells[2].Value = tableY[count_row];
+                }
+            }
+
+        }
+    }
+}
